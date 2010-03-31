d79e46949fdf056ed89b663e133af8031fdcce14
tstofflet <tstofflet@thig.com>
2010-03-31 17:30:26 -0400

    Added tabbed layout & activities for DLC search. Added some new drawables. Leaderboard layout is next.

--- src/com/android/rockband/DLCSearch.java
+++ src/com/android/rockband/DLCSearch.java
@@ -1,21 +1,41 @@
 package com.android.rockband;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
-public class DLCSearch extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
-    }
-    
-    @Override
+public class DLCSearch extends TabActivity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+		
+		Resources res = getResources();
+		TabHost tabHost = getTabHost();
+		TabHost.TabSpec spec;
+		Intent intent;
+		
+		intent = new Intent().setClass(this, RB2DLCSearch.class);
+		spec = tabHost.newTabSpec("ic_tab_rb2").setIndicator("RB2", res.getDrawable(R.drawable.ic_tab_rb2)).setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, TBRBDLCSearch.class);
+		spec = tabHost.newTabSpec("ic_tab_tbr").setIndicator("TB:RB", res.getDrawable(R.drawable.ic_tab_tbr)).setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, RBNDLCSearch.class);
+		spec = tabHost.newTabSpec("ic_tab_rbn").setIndicator("RBN", res.getDrawable(R.drawable.ic_tab_rbn)).setContent(intent);
+		tabHost.addTab(spec);
+		
+		tabHost.setCurrentTabByTag("ic_tab_rb2");
+	}
+	
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
     	MenuInflater inflater = getMenuInflater();
     	inflater.inflate(R.menu.main_menu, menu);
