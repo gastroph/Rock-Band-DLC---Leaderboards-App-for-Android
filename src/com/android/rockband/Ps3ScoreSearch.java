1b68a2b6f3da4b06681c1236ec8ebee018147d78
tstofflet <tstofflet@thig.com>
2010-04-01 17:03:54 -0400

    More layout stuff...

--- src/com/android/rockband/Ps3ScoreSearch.java
+++ src/com/android/rockband/Ps3ScoreSearch.java
@@ -2,14 +2,54 @@ package com.android.rockband;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class Ps3ScoreSearch extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.score_search);
 		
-		TextView textview = new TextView(this);
-		textview.setText("PS3 Leaderboard Search");
-		setContentView(textview);
+		Spinner gameSpinner = (Spinner) findViewById(R.id.game_spinner);
+		Spinner instrumentSpinner = (Spinner) findViewById(R.id.instrument_spinner);
+		
+		ArrayAdapter<CharSequence> gameSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.game_array, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> instrumentSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.instrument_array, android.R.layout.simple_spinner_item);
+		
+		gameSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		instrumentSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		gameSpinner.setAdapter(gameSpinnerAdapter);
+		gameSpinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+		instrumentSpinner.setAdapter(instrumentSpinnerAdapter);
+		instrumentSpinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+		
+		final EditText name = (EditText) findViewById(R.id.playerName);
+		name.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+					Toast.makeText(Ps3ScoreSearch.this, name.getText(), Toast.LENGTH_SHORT).show();
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+	
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+		public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+			Toast.makeText(parent.getContext(), "You Selected: " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+		}
+		
+		public void onNothingSelected(AdapterView<?> parent) {
+			Toast.makeText(parent.getContext(), "Please make a selection to continue.", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
